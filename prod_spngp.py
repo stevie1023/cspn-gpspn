b8bb1b9bc3e723e524475421ff3ec0b8f95eb479
Stevie Zhu <51037813+stevie1023@users.noreply.github.com>
2020-12-21 17:45:22 +0800

    Update prod_spngp.py

--- prod_spngp.py
+++ prod_spngp.py
@@ -49,65 +49,69 @@ opts = {
 root_region, gps_ = build_bins(**opts)
 
 root, gps = structure(root_region,scope = [i for i in range(y.shape[1])], gp_types=['rbf'])
-# lr = 0.1
-# steps = 200
-# likelihood_scope = [GaussianLikelihood().train(),GaussianLikelihood().train()]
-# tensor_x = torch.from_numpy(np.zeros((100,1))).float().to('cuda')
-# tensor_y = torch.from_numpy(np.zeros((100,1))).float().to('cuda')
-# model_scope = [ExactGPModel(x = tensor_x,y = tensor_y,likelihood = likelihood_scope[i], type='rbf_ard') for i in range(y.shape[1])]
-# l0=list(model_scope[0].parameters())
-# l0.extend(list(model_scope[1].parameters()))
-# for param in l0:
-#     print(f'value = {param.item()}')
-#
-# optimizer_scope = Adam([{'params':l0}], lr=lr)
+lr = 0.1
+steps = 200
+likelihood_scope = [GaussianLikelihood().train(),GaussianLikelihood().train()]
+tensor_x = torch.from_numpy(np.zeros((100,1))).float().to('cuda')
+tensor_y = torch.from_numpy(np.zeros((100,1))).float().to('cuda')
+model_scope = [ExactGPModel(x = tensor_x,y = tensor_y,likelihood = likelihood_scope[i], type='rbf_ard') for i in range(y.shape[1])]
+l0=list(model_scope[0].parameters())
+l0.extend(list(model_scope[1].parameters()))
+for param in l0:
+    print(f'value = {param.item()}')
 
-# model_scope = [i.to('cuda') for i in model_scope]
+optimizer_scope = Adam([{'params':l0}], lr=lr)
 
-# for i in range(steps): #这是优化的大循环，优化共#steps步
-#     tree_loss = [0] * y.shape[1]
-#     tree_scope = [0] * y.shape[1]
-#     optimizer_scope.zero_grad()
-#     for j, gp in enumerate(gps):
-#         idx = query(x, gp.mins, gp.maxs)
-#         gp.x = x[idx]
-#         y_scope = y[:,gp.scope]
-#         gp.y = y_scope[idx]
-#         cuda_ = True
-#         temp_device = torch.device("cuda" if cuda_ else "cpu")
-#         if cuda_:
-#             torch.cuda.empty_cache()
-#         x_temp = torch.from_numpy(gp.x).float().to(temp_device)
-#         y_temp = torch.from_numpy(gp.y.ravel()).float().to(temp_device)
-#         model_scope[gp.scope].set_train_data(inputs=x_temp, targets=y_temp, strict=False)
-#         model_scope[gp.scope].train()
-#         mll = ExactMarginalLogLikelihood(likelihood_scope[gp.scope], model_scope[gp.scope])
-#         # for param_name, param in mll.named_parameters():
-#         #     print(f'Parameter namemll0: {param_name:42} value = {param.item()}')
-#         output = model_scope[gp.scope](x_temp)  # Output from model
-#         loss = -mll(output, y_temp)
-#         x_temp.detach()
-#         y_temp.detach()
-#         del x_temp
-#         del y_temp
-#         x_temp = y_temp = None
-#         torch.cuda.empty_cache()
-#         gc.collect()
-#         tree_loss[gp.scope] += loss #计算并累加每个叶子的loss
-#         tree_scope[gp.scope] += 1
+model_scope = [i.to('cuda') for i in model_scope]
 
-#     tree_loss_all = tree_loss[0]+tree_loss[1]
-#     print('loss',tree_loss_all.item())
+for i in range(steps): #这是优化的大循环，优化共#steps步
+    # tree_loss = [0] * y.shape[1]
+    # tree_scope = [0] * y.shape[1]
+    optimizer_scope.zero_grad()
+    for j, gp in enumerate(gps):
+        idx = query(x, gp.mins, gp.maxs)
+        gp.x = x[idx]
+        y_scope = y[:,gp.scope]
+        gp.y = y_scope[idx]
+        cuda_ = True
+        temp_device = torch.device("cuda" if cuda_ else "cpu")
+        if cuda_:
+            torch.cuda.empty_cache()
+        x_temp = torch.from_numpy(gp.x).float().to(temp_device)
+        y_temp = torch.from_numpy(gp.y.ravel()).float().to(temp_device)
+        model_scope[gp.scope].set_train_data(inputs=x_temp, targets=y_temp, strict=False)
+        model_scope[gp.scope].train()
+        mll = ExactMarginalLogLikelihood(likelihood_scope[gp.scope], model_scope[gp.scope])
 
-#     tree_loss_all.backward()
-#     optimizer_scope.step()
-#   
-# for param_name, param in model_scope[0].named_parameters():
-#     print(f'Parameter name0: {param_name:42} value = {param.item()}')
-# for param_name, param in model_scope[1].named_parameters():
-#     print(f'Parameter name1: {param_name:42} value = {param.item()}')
-# for param_name, param in model_scope[2].named_parameters():
-#     print(f'Parameter name2: {param_name:42} value = {param.item()}')
+        output = model_scope[gp.scope](x_temp)  # Output from model
+        gp.mll = mll(output, y_temp).item()
+        gp.mll_grad = mll(output, y_temp)
+        # loss = -mll(output, y_temp)
+        x_temp.detach()
+        y_temp.detach()
+        del x_temp
+        del y_temp
+        x_temp = y_temp = None
+        torch.cuda.empty_cache()
+        gc.collect()
+        # tree_loss[gp.scope] += loss #计算并累加每个叶子的loss
+        # tree_scope[gp.scope] += 1
+
+    # tree_loss_all = tree_loss[0]+tree_loss[1]
+    # print('loss',tree_loss_all.item())
+    root.update()
+    tree_loss_all = -root.update_mll()
+    print('loss', tree_loss_all[0].item())
+    tree_loss_all[0].backward()
+    optimizer_scope.step()
+root.update()
+  
+for param_name, param in model_scope[0].named_parameters():
+    print(f'Parameter name0: {param_name:42} value = {param.item()}')
+for param_name, param in model_scope[1].named_parameters():
+    print(f'Parameter name1: {param_name:42} value = {param.item()}')
+for param_name, param in model_scope[2].named_parameters():
+    print(f'Parameter name2: {param_name:42} value = {param.item()}')
 #
 # for i, gp in enumerate(gps):
 #     x_temp = torch.from_numpy(gp.x).float().to('cuda')
